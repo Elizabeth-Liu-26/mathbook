c5ffdc8d04512cf937d736bc78ed13d155ac6091
Elizabeth Liu <ensumner@gmail.com>
2022-11-16 13:52:07 -0500

    trying again p1

---
+++ _build/jupyter_execute/Practice Test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Chapter 3
+# ### 3.3 Derivatives of Trig Functions
+# 
+# Find the derivative:
+# 
+# $y=1-sec(x)tan x$
+#  
+# $y=csc  ( + cot )$
+# 
+# 
+
+# In[ ]:
+
+
+
+
