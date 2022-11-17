8c1de8268a9e54a3000c0300c3c94d046f8ed1a2
Elizabeth Liu <ensumner@gmail.com>
2022-11-17 13:44:02 -0500

    Testing Dropdown

---
+++ _build/jupyter_execute/Graphing Functions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Using Derivatives to Graph Functions
+
+# #### First, some definitions
+
+# ```{dropdown} The function is <b> positive </b>.
+# For a given $x$ , $f(x) > 0$. aka the value of the function, at that point, is a positive number. 
+# 
+# Similarly, if the function is <b> negative </b>, the function is a negative number. (For a given $x$ , $f(x) < 0$.
+# ```
+
+# In[ ]:
+
+
+
+
