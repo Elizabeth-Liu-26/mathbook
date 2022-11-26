8695e9a43949b6bf90fe2c33c62fa535255b3b79
Elizabeth Liu <ensumner@gmail.com>
2022-11-26 14:04:33 -0500

    Updating Intro

--- _build/jupyter_execute/Graphing Functions.py
+++ _build/jupyter_execute/Graphing Functions.py
@@ -17,7 +17,8 @@
 # Similarly, if the function is <b> decreasing </b>, as $x$ increases (gets bigger), $f(x)$ decreases (gets smaller). "going downhill"
 # ```
 
-# ```{note}
+# ```{admonition} Note
+# :class: dropdown
 # Note that we define positive/negative for the function at a give point ($x$). Whereas we define increasing/decreasing for the function on a given interval ([$x_1,x_2$])
 # ```
 
@@ -27,21 +28,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+{
+    "tags": [
+        "hide-input",
+    ]
+}
+
 
 # In[2]:
 
 
+fig, ax = plt.subplots()
+
 x = np.arange(-2,6,.01)
 y = .25*(x**3-6*x**2)+4
+ax.plot(x,y,color="#999B84")
 
+#adding lines at x=0, y=0
+y_line = 0
+ax.axhline(y_line, color='gray', linestyle='dashed')
+x_line = 0
+ax.axvline(x_line, color='gray', linestyle='dashed')
 
-# In[3]:
+ax.text(-1.5, 2, "Pos. & Inc.", ha="center", va="center", rotation=70, size=10,
+        bbox=dict(boxstyle="rarrow,pad=0.3", fc="#EFD9D1", ec="black", lw=1))
 
+ax.text(1.65, 2.5, "Pos. & Dec.", ha="center", va="center", rotation=-58, size=10,
+        bbox=dict(boxstyle="rarrow,pad=0.3", fc="#EFD9D1", ec="black", lw=1))
 
-plt.plot(x,y)
-plt.ylabel('some numbers')
+ax.text(2.4, -2.5, "Neg. & Dec.", ha="center", va="center", rotation=-58, size=10,
+        bbox=dict(boxstyle="rarrow,pad=0.3", fc="#DDB7AB", ec="black", lw=1))
+
+ax.text(5.4, -2.5, "Neg. & Inc.", ha="center", va="center", rotation=66, size=10,
+        bbox=dict(boxstyle="rarrow,pad=0.3", fc="#DDB7AB", ec="black", lw=1))
+
 plt.show()
+{
+    "tags": [
+        "hide-input",
+    ]
+}
 
+
+# ### First Derivative Test
+
+# ```{admonition} Note
+# :class: dropdown
+# Remember that local maxmima and minima are critical numbers, but not all critical numbers need to be a maximum or minimum.
+# ```
 
 # In[ ]:
 
--- intro.md
+++ intro.md
@@ -1,11 +1,15 @@
-# Welcome to your Jupyter Book
+# Welcome to Mathbook
 
-This is a small sample book to give you a feel for how book content is
-structured.
-It shows off a few of the major file types, as well as some sample content.
-It does not go in-depth into any particular topic - check out [the Jupyter Book documentation](https://jupyterbook.org) for more information.
+I'm glad you're here! 
 
-Check out the content pages bundled with this sample book to see more.
+There are many, many amazing math resources out there, but sometimes it can be a bit overwhelming to find and parse through them all. Mathbook is meant to be an initial landing page - your starting point to explore, asses what you know, and grow curiosity about the wonders of math. 
+
+Topics are covered briefly and at a high-level, with pleny of practice problems to try and additional helpful resources linked throughout. If you think of your school textbook as the main course, consider this site a delicious appetizer - a fun intro, but not enough to fill you up all the way.
+
+Mathbook is a passion project and labor of love after years of tutoring so many amazing students of math. I'm constantly adding new topics - and if you'd like to suggest something (or just say hi) please send an email to mathbook@gmail.com. 
+I hope you find this site useful. Don't be shy to explore new topics, and above all remember: <span style="font-family:Papyrus; font-size:1em;"><i>Anyone and everyone can learn, love, and live the beauty of math.</i></span>
+
+-Elizabeth
 
 ```{tableofcontents}
 ```
